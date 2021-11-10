6f1e6658b99de222c46c51f72b77fa79be78a3d7
Gerardo <gcerpa02@cox.net>
2021-11-10 11:38:04 -0800

    Initial Commit

---
+++ ColorWheel_Test/ColorWheel_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorWheel_TestApp.swift
+//  ColorWheel_Test
+//
+//  Created by Gerardo on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct ColorWheel_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
